7c82c3660428e4e8590c46f0d6f4a9a936f7bafb
T-rex007 <shaqc777@yahoo.com>
2021-02-26 17:27:26 -0400

    -------

--- cmd.py
+++ cmd.py
@@ -1,6 +1,12 @@
 import sys
+import argparse
 
+parser = argparse.ArgumentParser()
 
+assertion_statment ="Ooops you may have entered an invalid bot mode\n\tAvailible modes are 'live', 'demo' and 'test'"
+parser.add_argument("mode", help ="Opening Bot in demo mode", type = str)
+args = parser.parse_args()
+assert((args.mode == 'demo')|(args.mode =='live')|(args.mode=='test')), assertion_statment
 
 if __name__ == '__main__' :
-    print(len(sys.argv[:]))
+    print('Runnin Bot in {} mode'.format(args.mode))
